62eb458c97fbd4a79c2d6a33a6155697b2b7504b
Abhinav123-1 <abhinav.chintale@gmail.com>
2024-03-23 10:11:09 +0530

    "Formatted Changes"

--- com/abhinav/array/basic/RotateArrayByK.java
+++ com/abhinav/array/basic/RotateArrayByK.java
@@ -4,25 +4,22 @@ import java.util.LinkedList;
 
 public class RotateArrayByK {
     public static void main(String[] args) {
-        int [] nums = {1,2,3,4,5,6,7};
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
         RotateArrayByKSolution rabks = new RotateArrayByKSolution();
         rabks.rotate(nums, 3);
     }
-    
+
 }
 
-class RotateArrayByKSolution{
+class RotateArrayByKSolution {
     public void rotate(int[] arr, int k) {
         LinkedList<Integer> ll = new LinkedList<>();
-        for(int num: arr){
-            ll.offer(num); //this uses queue
+        for (int num : arr) {
+            ll.offer(num); // this uses queue
         }
-        for(int i=0;i<arr.length;i++){
-            arr[(i+k)%arr.length] = ll.poll();
+        for (int i = 0; i < arr.length; i++) {
+            arr[(i + k) % arr.length] = ll.poll();
         }
-    
-       
 
-       
-   }
+    }
 }
