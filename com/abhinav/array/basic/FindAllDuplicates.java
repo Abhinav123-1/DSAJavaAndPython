e4cd94cf7e7346cf4d6a96e63cf4b796979c55fc
Abhinav123-1 <abhinav.chintale@gmail.com>
2024-03-25 17:59:25 +0530

    Superior Element no exactly Brute Force one test case fail

---
+++ com/abhinav/array/basic/FindAllDuplicates.java
@@ -0,0 +1,28 @@
+package com.abhinav.array.basic;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindAllDuplicates {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class FindAllDuplicatesSolution{
+    public List<Integer> findDuplicates(int[] nums) {
+        Map<Integer,Integer> duMap = new HashMap<>();
+        List<Integer> duList = new ArrayList<>();
+        for(int num:nums){
+            if(duMap.containsKey(num)){
+                duList.add(num);
+            }else{
+                duMap.putIfAbsent(num, 1);
+            }
+
+        }
+        return duList;
+    }
+}
--- com/abhinav/array/basic/SuperiorElement.java
+++ com/abhinav/array/basic/SuperiorElement.java
@@ -1,13 +1,12 @@
 package com.abhinav.array.basic;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class SuperiorElement {
     public static void main(String[] args) {
-        int a[] = {5,4,3};
+        int a[] = {1,2,3,4,9,8,7,6,5};
         SuperiorElementSolution ses = new SuperiorElementSolution();
         System.out.println(ses.superiorElements(a));
         
@@ -19,15 +18,15 @@ class SuperiorElementSolution{
         //1 2 3 4 9 8 7 6 5 
       List<Integer> superiorElemetList = new ArrayList<>();
       int  highestPt = 0;
-        while(highestPt<a.length-1){
+        while(highestPt<=a.length-1){
             int madMax = Integer.MIN_VALUE;
-            for(int i = highestPt == 0 ? highestPt : highestPt+1  ; i<a.length;i++){
+            for(int i = highestPt  ; i<a.length;i++){
                 madMax  = Math.max(madMax,a[i]);
                 if(madMax<=a[i]){
                     highestPt = i;
                 }
             }
-            highestPt+=1;
+            highestPt++;
             superiorElemetList.add(madMax); 
         }    
          Collections.sort(superiorElemetList);
