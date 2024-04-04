3ab65ac103e8363f1ca0dc091900d93fecb0c234
Abhinav123-1 <abhinav.chintale@gmail.com>
2024-04-05 01:24:10 +0530

    Search in Rotated array

--- com/abhinav/array/basic/SaerchInRotSortArr.java
+++ com/abhinav/array/basic/SaerchInRotSortArr.java
@@ -2,43 +2,36 @@ package com.abhinav.array.basic;
 
 public class SaerchInRotSortArr {
     public static void main(String[] args) {
-        int nums[] = {4,5,6,7,0,1,2};
+        int nums[] = { 4, 5, 6, 7, 0, 1, 2 };
         int target = 0;
         SaerchInRotSortArrSolution sirsas = new SaerchInRotSortArrSolution();
-        System.out.println(sirsas.search(nums,target));  
+        System.out.println(sirsas.search(nums, target));
     }
 }
 
-class SaerchInRotSortArrSolution{
+class SaerchInRotSortArrSolution {
     public int search(int[] nums, int target) {
-        int n =nums.length;
+        int n = nums.length;
         int left = 0;
-        int right = n-1;
+        int right = n - 1;
         int mid;
-    
-        while(left<=right)
-        {
-            mid = (left+right)/2;
-            
-            if(nums[mid]==target)
-            return mid;
-    
-            else if(nums[mid]>=nums[left])
-            {
-                if(target<=nums[mid] && target>=nums[left])
-                right = mid-1;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] >= nums[left]) {
+                if (target <= nums[mid] && target >= nums[left])
+                    right = mid - 1;
                 else
-                left = mid+1;
+                    left = mid + 1;
+            } else {
+                if (target >= nums[mid] && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
             }
-            else
-            {
-                if(target>=nums[mid]&& target<=nums[right])
-                left = mid+1;
-                else
-                right = mid-1; 
-            }   
         }
         return -1;
-    
+
     }
 }
