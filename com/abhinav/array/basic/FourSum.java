83f5cb7d620b1b2d864c43e9e39d49ef6648f2f3
Abhinav123-1 <abhinav.chintale@gmail.com>
2024-04-07 00:06:35 +0530

    Three Sum

---
+++ com/abhinav/array/basic/FourSum.java
@@ -0,0 +1,19 @@
+package com.abhinav.array.basic;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class FourSumSolution{
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums); 
+        return null;
+
+        
+    }
+}
--- com/abhinav/array/basic/ThreeSum.java
+++ com/abhinav/array/basic/ThreeSum.java
@@ -16,13 +16,36 @@ public class ThreeSum {
 
 class ThreeSumSolution{
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<Integer> sumList  = new ArrayList<>();
-        List<List<Integer>> combinedList = new ArrayList<>();
-        int k = 0;
-        int sum = 0;
-        //-4,-1,-1,0,1,2 
-        
-        return combinedList; 
+        Arrays.sort(nums); 
+        List<List<Integer>> combinedlist = new ArrayList<>();
+        int length = nums.length;
+        for (int low = 0; low < length - 2 && nums[low] <= 0; ++low) {
+            if (low > 0 && nums[low] == nums[low - 1]) {
+                continue;
+            }
+            int mid = low + 1;
+            int high = length - 1;
+            while (mid < high) {
+                int sum = nums[low] + nums[mid] + nums[high];
+                if (sum < 0) {
+                    ++mid;
+                }
+                else if (sum > 0) {
+                    --high;
+                }
+                else {
+                    combinedlist.add(List.of(nums[low], nums[mid], nums[high]));
+                    while (mid < high && nums[mid] == nums[mid + 1]) {
+                        ++mid;
+                    }
+                    while (mid < high && nums[high] == nums[high - 1]) {
+                        --high;
+                    }
+                    ++mid;
+                    --high;
+                }
+            }
+        }
+        return combinedlist;
     }
 }
