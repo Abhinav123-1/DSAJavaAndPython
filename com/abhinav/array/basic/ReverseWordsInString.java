de6c07a105eb82652f6d60e1f37c62cd051e138a
Abhinav123-1 <abhinav.chintale@gmail.com>
2024-04-07 01:00:27 +0530

    Reverse Word String

---
+++ com/abhinav/array/basic/ReverseWordsInString.java
@@ -0,0 +1,23 @@
+package com.abhinav.array.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseWordsInString {
+    public static void main(String[] args) {
+        String s = "a good   example";
+        ReverseWordsInStringSolution rwiss = new ReverseWordsInStringSolution();
+        System.out.println(rwiss.reverseWords(s));
+    }
+}
+
+class ReverseWordsInStringSolution {
+    public String reverseWords(String s) {
+        String[] words = s.trim().replaceAll(" +", " ").split(" ");
+        StringBuilder reverse = new StringBuilder(" ");
+        for (int i = words.length - 1; i >= 0; i--) {
+            reverse.append(words[i] + " ");
+        }
+        return reverse.toString().trim();
+    }
+}
